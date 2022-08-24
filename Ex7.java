c56f4f82059c9ff1792f20803ec621916710ef65
Anfhajj <111368996+Anfhajj@users.noreply.github.com>
2022-08-24 16:19:20 +0300

    Add files via upload

---
+++ Ex7.java
@@ -0,0 +1,10 @@
+public class Ex7 {
+    public static void main(String[] args) {
+   String str="Hello World";
+        System.out.println(str.charAt(0));
+        System.out.println(str.substring(1,11));
+        System.out.println(str.charAt(10));
+        System.out.println(str.substring(0,10));
+
+    }
+}
